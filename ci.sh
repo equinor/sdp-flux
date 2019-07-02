71a101f4d07a5dcdfa3e6a737886690b157b9e77
soofstad <stigofstad@gmail.com>
2019-07-02 09:47:02 +0200

    CI test

--- ci.sh
+++ ci.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 if [ "$1" = "charts" ]; then
+    echo "Linting custom charts"
     for chart in $(ls ./custom-charts); do
       helm lint ./custom-charts/${chart}
       if [ $? != 0 ]; then
@@ -8,13 +9,18 @@ if [ "$1" = "charts" ]; then
       fi
      done
 elif [ "$1" = "kustomize" ]; then
+    echo "Trying to build kustomize"
     ./kubectl kustomize development/
     ./kubectl kustomize production/
 elif [ "$1" = "manifests" ]; then
+    echo "Linting K8s manifests"
     STANDARD_MANIFESTS=$(find "$(pwd -P)" -not -path "*/custom-charts*" -type f \( ! -iname ".flux.yaml" \) -name *.yaml -exec grep -L -H 'apiVersion: flux.weave.works/v1beta1\|apiVersion: bitnami.com\|apiVersion: certmanager.k8s.io\|apiVersion: kustomize.config.k8s.io/v1beta1' {} \;)
 
     ./kubeval $STANDARD_MANIFESTS
       if [ $? != 0 ]; then
        travis_terminate 1
       fi
+else
+    echo "Error: Invalid argument"
+    exit 1
 fi
