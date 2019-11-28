36ed930e29a71c24a4314b0098a8c11304248877
Jostein Solaas <josas@statoil.com>
2019-11-28 12:13:38 +0100

    fix kustomization

--- ci.sh
+++ ci.sh
@@ -10,8 +10,9 @@ if [ "$1" = "charts" ]; then
      done
 elif [ "$1" = "kustomize" ]; then
     echo "Trying to build kustomize"
-    ./kubectl kustomize development/
-    ./kubectl kustomize production/
+    ./kubectl kustomize dev/
+    ./kubectl kustomize prod/
+    ./kubectl kustomize nor/
 elif [ "$1" = "manifests" ]; then
     echo "Linting K8s manifests"
     STANDARD_MANIFESTS=$(find "$(pwd -P)" -not -path "*/custom-charts*" -type f \( ! -iname ".flux.yaml" \) -name *.yaml -exec grep -L -H 'apiVersion: flux.weave.works/v1beta1\|apiVersion: bitnami.com\|apiVersion: certmanager.k8s.io\|apiVersion: kustomize.config.k8s.io/v1beta1\|apiVersion: ceph.rook.io/v1' {} \;)
