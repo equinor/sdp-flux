dca10208e1eb80f05d64d9b979b48f60ec92edd6
Audun L. Solemdal <auls@equinor.com>
2019-12-17 17:48:29 +0100

    Remove nor/ reference from kustomize

--- ci.sh
+++ ci.sh
@@ -12,7 +12,6 @@ elif [ "$1" = "kustomize" ]; then
     echo "Trying to build kustomize"
     ./kubectl kustomize dev/
     ./kubectl kustomize prod/
-    ./kubectl kustomize nor/
 elif [ "$1" = "manifests" ]; then
     echo "Linting K8s manifests"
     STANDARD_MANIFESTS=$(find "$(pwd -P)" -not -path "*/custom-charts*" -type f \( ! -iname ".flux.yaml" \) -name *.yaml -exec grep -L -H 'apiVersion: flux.weave.works/v1beta1\|apiVersion: bitnami.com\|apiVersion: certmanager.k8s.io\|apiVersion: kustomize.config.k8s.io/v1beta1\|apiVersion: ceph.rook.io/v1' {} \;)
