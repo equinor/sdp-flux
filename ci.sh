c5e762117d5f9b76727ae88878b4f842e84d049b
Audun Solemdal <auls@equinor.com>
2020-05-04 15:09:34 +0200

    Various version bumps into prod (#73)
    * verdaccio: update for token endpoint (#70)
    
    Authored-by: Jostein Solaas <josas@statoil.com>
    
    * dev bumps into dev branch (#72)
    
    * velero: bump to latest version
    
    * sealed-secrets: bump to latest version
    
    * nginx: bump to latest chart version
    
    * oauth2-proxy: bump to latest version 3.0.0
    
    * travis-ci: fix, update and improve jobs
    
    Co-authored-by: Jostein Solaas <33114722+jsolaas@users.noreply.github.com>

--- ci.sh
+++ ci.sh
@@ -5,19 +5,39 @@ if [ "$1" = "charts" ]; then
     for chart in $(ls ./custom-charts); do
       helm lint ./custom-charts/${chart}
       if [ $? != 0 ]; then
-       travis_terminate 1
+    printf '%d\n' $? && echo "was the return code.. exiting.."
+       exit 1
       fi
      done
 elif [ "$1" = "kustomize" ]; then
-    echo "Trying to build kustomize"
+    echo "kustomize + kubeval validation for dev cluster:"
+
+    ./kubectl kustomize ./dev  | ./kubeval --strict --ignore-missing-schemas
+
+    if [ $? != 0 ]; then
+       printf '%d\n' $? && echo "was the return code.. exiting.."
+       exit 1
+      fi
+
+    echo "kustomize + kubeval validation for prod cluster:"
+    ./kubectl kustomize ./prod  | ./kubeval --strict --ignore-missing-schemas
+
+    if [ $? != 0 ]; then
+       printf '%d\n' $? && echo "was the return code.. exiting.."
+       exit 1
+      fi
+
+    echo "Displaying YAML output for dev cluster:"
     ./kubectl kustomize dev/
+    echo "Displaying YAML output for prod cluster:"
     ./kubectl kustomize prod/
 elif [ "$1" = "manifests" ]; then
-    echo "Linting K8s manifests, CRDs are ignored"
-    MANIFESTS=$(find "$(pwd -P)" -not -path "*/custom-charts*" -type f \( ! -iname ".flux.yaml" \) -name "*.yaml" )  
-    ./kubeval --ignore-missing-schemas $MANIFESTS
+    echo "Linting K8s manifests from base only."
+    # Only lint ./base because partial patches in ./prod and ./dev give false positives. 
+    ./kubeval -d ./base --ignore-missing-schemas --ignored-filename-patterns flux.yaml,kustomization.yaml
       if [ $? != 0 ]; then
-       travis_terminate 1
+       printf '%d\n' $? && echo "was the return code.. exiting.."
+       exit 1
       fi
 else
     echo "Error: Invalid argument"
