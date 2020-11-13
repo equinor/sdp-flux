e3869f9e0d76464ad42acc0222758512343ebb8d
Stig Ofstad <stigofstad@gmail.com>
2020-11-13 08:05:52 +0100

    hgir: downscale in dev

--- ci.sh
+++ ci.sh
@@ -12,7 +12,7 @@ if [ "$1" = "charts" ]; then
 elif [ "$1" = "kustomize" ]; then
     echo "kustomize + kubeval validation for dev cluster:"
 
-    ./kubectl kustomize ./dev  | ./kubeval --strict --ignore-missing-schemas
+    kubectl kustomize ./dev  | kubeval --strict --ignore-missing-schemas
 
     if [ $? != 0 ]; then
        printf '%d\n' $? && echo "was the return code.. exiting.."
@@ -20,7 +20,7 @@ elif [ "$1" = "kustomize" ]; then
       fi
 
     echo "kustomize + kubeval validation for prod cluster:"
-    ./kubectl kustomize ./prod  | ./kubeval --strict --ignore-missing-schemas
+    kubectl kustomize ./prod  | kubeval --strict --ignore-missing-schemas
 
     if [ $? != 0 ]; then
        printf '%d\n' $? && echo "was the return code.. exiting.."
@@ -28,13 +28,13 @@ elif [ "$1" = "kustomize" ]; then
       fi
 
     echo "Displaying YAML output for dev cluster:"
-    ./kubectl kustomize dev/
+    kubectl kustomize dev/
     echo "Displaying YAML output for prod cluster:"
-    ./kubectl kustomize prod/
+    kubectl kustomize prod/
 elif [ "$1" = "manifests" ]; then
     echo "Linting K8s manifests from base only."
-    # Only lint ./base because partial patches in ./prod and ./dev give false positives. 
-    ./kubeval -d ./base --ignore-missing-schemas --ignored-filename-patterns flux.yaml,kustomization.yaml
+    # Only lint ./base because partial patches in ./prod and ./dev give false positives.
+    kubeval -d ./base --ignore-missing-schemas --ignored-filename-patterns flux.yaml,kustomization.yaml
       if [ $? != 0 ]; then
        printf '%d\n' $? && echo "was the return code.. exiting.."
        exit 1
